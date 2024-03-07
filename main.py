abb8e4040cb5b162bf3b88dfc3a3b57eaee8cb61
Rufus Knight <rknight.dev@gmail.com>
2024-03-07 14:39:02 -0500

    Attempt in trying to group logs by userid

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import json
 import pprint
 #import logging
 from datetime import datetime
+from datetime import timedelta
 
-import apache_beam as beam
 from beam_nuggets.io.kafkaio import KafkaConsume
-from apache_beam import Pipeline, Map, GroupByKey, ParDo, WindowInto
-from apache_beam.transforms.window import FixedWindows
-from apache_beam.transforms.trigger import Always
+from apache_beam import Pipeline, Map, GroupByKey, CombinePerKey, DoFn, ParDo, WindowInto, PTransform
+from apache_beam.transforms.window import FixedWindows, TimestampedValue, Sessions, GlobalWindows, SlidingWindows
+from apache_beam.transforms.trigger import AfterWatermark, AfterEach, Repeatedly, AfterAny, AfterProcessingTime, AfterCount, AccumulationMode
 from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions, StandardOptions
 
-from apache_beam import window
+from apache_beam.transforms.combiners import ToListCombineFn
 
 class MSKTokenProvider():
     def token(self):
@@ -18,6 +18,51 @@ class MSKTokenProvider():
 
         token, _ = MSKAuthTokenProvider.generate_auth_token(region)
         return token
+
+class LogProcessor(DoFn):
+    def process(self, log, timestamp=DoFn.TimestampParam, window=DoFn.WindowParam):
+        if log[0] is not None:
+            json_log = json.loads(log[1])
+            page_url = json_log["pageUrl"] if "pageUrl" in json_log and isinstance(json_log["pageUrl"], str) else None
+            log_type = json_log["type"] if "type" in json_log and isinstance(json_log["type"], str) else None
+
+            if (not (page_url is None or log_type is None) and
+                str(page_url).lower().find("openstreetmap") > -1 and str(log_type).lower() == "visit" and
+                "details" in json_log and "name" in json_log["details"] and isinstance(json_log["details"]["name"], str)):
+                yield {"key": log[0], "value": (int(json_log["clientTime"]), json_log["details"]["name"])}
+                #return (log[0], (int(json_log["clientTime"]), json_log["details"]["name"]))
+
+class Aggregate(PTransform):
+  """Calculates scores for each team within the configured window duration.
+
+  Extract team/score pairs from the event stream, using hour-long windows by
+  default.
+  """
+  def __init__(self, team_window_duration, allowed_lateness):
+    # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
+    # super().__init__()
+    PTransform.__init__(self)
+    self.team_window_duration = team_window_duration * 60
+    self.allowed_lateness_seconds = allowed_lateness * 60
+
+  def expand(self, pcoll):
+    # NOTE: the behavior does not exactly match the Java example
+    # TODO: allowed_lateness not implemented yet in FixedWindows
+    # TODO: AfterProcessingTime not implemented yet, replace AfterCount
+    return (
+        pcoll
+        # We will get early (speculative) results as well as cumulative
+        # processing of late data.
+        | 'LeaderboardTeamFixedWindows' >> WindowInto(
+            FixedWindows(self.team_window_duration),
+            trigger=AfterWatermark(AfterCount(10), AfterCount(20)),
+            accumulation_mode=AccumulationMode.ACCUMULATING)
+    )
+
+class AddTimestamp(DoFn):
+    def process(self, elem):
+        yield TimestampedValue(elem, round(elem["value"][0] / 1000))
+        #return TimestampedValue(elem, time.time())
 
 def get_secret():
     import boto3
@@ -45,79 +90,69 @@ def get_secret():
             return get_secret_value_response['SecretBinary']
 
 def print_element(elem):
-    pp.pprint(f"{elem}\n")
+    pp.pprint(elem)
     return elem
 
-def filter_out_nomes(log):
-    if log[0] is not None:
-        json_log = json.loads(log[1])
-        page_url = json_log["pageUrl"] if "pageUrl" in json_log else None
-        log_type = json_log["type"] if "type" in json_log else None
-
-        if (not (page_url is None or log_type is None) and
-            isinstance(page_url, str) and str(page_url).lower().find("openstreetmap") > -1 and
-            isinstance(log_type, str) and str(log_type).lower() == "visit"):
-            yield log
-
-def extract_name(elem):
-    json_log = json.loads(elem[1])
-    if "details" in json_log and "name" in json_log["details"]:
-        yield json_log["details"]["name"]
-
-def test(elem):
-    print(elem)
-    return elem
-
+count = 0
 region = "us-east-1"
 date_tag = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+
+# Sliding windows of 60 seconds every 60 seconds
+window_size = timedelta(seconds=60).total_seconds()  # in seconds
+window_period = timedelta(seconds=60).total_seconds()  # in seconds
 
 pp  = pprint.PrettyPrinter(indent=4, width=100)
 
 pl_options = PipelineOptions([], **{
     "region": region,
-    "parallelism": 2,
+    "parallelism": 1,
     #"runner": "FlinkRunner",
+    "runner": "DirectRunner",
     "job_name": f"tap_filter_{date_tag}",
 })
 
 pl_options.view_as(StandardOptions).streaming = True
 pl_options.view_as(SetupOptions).save_main_session = False #Must be set to "False" to prevent pickling issues
 
-with Pipeline(options=pl_options) as pipeline:
-    user_tags = (
-        pipeline
-        | "read from topic" >> KafkaConsume(
-            consumer_config={
-                "topic": "raw-logs",
-                "enable_auto_commit": "True",
-                'auto_offset_reset': 'earliest',
-                "security_protocol": "SASL_SSL",
-                "sasl_mechanism": "OAUTHBEARER",
-                "sasl_oauth_token_provider": MSKTokenProvider(),
-                "bootstrap_servers": json.loads(get_secret())['MSK_BROKERS'],
-                #NOTE: Setting "group_id" to a unique value each run will ensure that the offset
-                #will return to the earliest value and all records will be retrieved. If "group_id"
-                #stays constant then only the latest records will be retrieved
-                "group_id": f"raw-logs-reader_{date_tag}",
-                #"enable.auto.commit": "False",
-                #"consumer.timeout.ms": '15000'
-            }
-        )
-        #| "agg every 60 sec" >> WindowInto(
-        #    windowfn=FixedWindows(60),
-        #    trigger=AfterWatermark(
-        #        late=AfterCount(1),
-        #        early=AfterProcessingTime(delay=1 * 60)
-        #    ),
-        #    accumulation_mode=AccumulationMode.ACCUMULATING
-        #)
-        | "filter out NOME logs" >> ParDo(filter_out_nomes)
-        | "Extract annotation name" >> ParDo(extract_name)
-        # TODO: extract log timestamp for windowing (currently beam ingestion time)
-        | 'window' >> beam.WindowInto(window.Sessions(10 * 60)) # 10 min dead time (no visit) per user
-        # TODO: call prediction model when window closes
-        | "test" >> ParDo(test)
+with Pipeline(options=pl_options) as pipeline: (
+    pipeline
+    | "read from topic" >> KafkaConsume(
+        consumer_config={
+            "topic": "raw-logs",
+            "enable_auto_commit": "False",
+            'auto_offset_reset': 'earliest',
+            "security_protocol": "SASL_SSL",
+            "sasl_mechanism": "OAUTHBEARER",
+            "sasl_oauth_token_provider": MSKTokenProvider(),
+            "bootstrap_servers": json.loads(get_secret())['MSK_BROKERS'],
+            #NOTE: Setting "group_id" to a unique value each run will ensure that the offset
+            #will return to the earliest value and all records will be retrieved. If "group_id"
+            #stays constant then only the latest records will be retrieved
+            "group_id": f"raw-logs-reader_{date_tag}",
+            #"enable.auto.commit": "False",
+            #"consumer.timeout.ms": '15000'
+        },
     )
+    | "filter out NOME logs" >> ParDo(LogProcessor())
+    | 'Add timestamp' >> ParDo(AddTimestamp())
+    
+    #| "aggregate" >> Aggregate(10, 0)
+    | 'windowing' >> WindowInto(
+        windowfn=FixedWindows(10),
+        trigger=Repeatedly(AfterWatermark(AfterProcessingTime(10))),
+        accumulation_mode=AccumulationMode.ACCUMULATING
+    )
+   
+    #| "agg every 60 sec" >> WindowInto(
+    #    FixedWindows(10)
+    #    #windowfn=GlobalWindows(),
+    #    #trigger=Repeatedly(AfterProcessingTime(10)),
+    #    #accumulation_mode=AccumulationMode.ACCUMULATING
+    #)
+    #| '10 sec session' >> WindowInto(Sessions(10))
+    | 'group by' >> GroupByKey()
+    | "test2" >> ParDo(print_element)
+)
 
 #(
 #    user_tags
